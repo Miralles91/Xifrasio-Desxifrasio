d0470a678c5eb3e9366810c89d7ff3fbcf2dc153
Miralles91 <43479445+Miralles91@users.noreply.github.com>
2019-02-07 18:26:02 +0100

    Add files via upload

---
+++ GenerarFitxers/DesgenerarFitxers/FrmDesxifasio.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DesgenerarFitxers
+{
+    public partial class FrmDesxifasio : Form
+    {
+        public FrmDesxifasio()
+        {
+            InitializeComponent();
+        }
+        private void crearFitxers()
+        {
+            string rutaMirallesTonto = "C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\Huilaliem.txt";
+            String[] Codinumero = new String[1000000];
+            for (int i = 1; i < 5; i++)
+            {
+                DesxifrarNumLletra(i, Codinumero);
+            }
+            concatArxius(rutaMirallesTonto);
+
+            for (int z = 1; z < 5; z++)
+            {
+
+                File.Delete("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM" + z + ".txt");
+            }
+        }
+        public string[] CodiLletra()
+        {
+            SqlConnection connexxion = new SqlConnection();
+            connexxion.ConnectionString = ConfigurationManager.ConnectionStrings["RepublicSystemConnectionString"].ConnectionString;
+            connexxion.Open();
+
+            DataSet dtsCli = new DataSet();
+
+            string query = "SELECT Numbers from InnerEncryptionData where IdInnerEncryption = 24"; // and b.Day = '" + Convert.ToDateTime(DateTime.Today).ToString("yyyy-MM-dd") + "'";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connexxion);
+            adapter.Fill(dtsCli);
+
+            string[] LletraCodi = new string[26];
+
+            for (int i = 0; i < LletraCodi.Length; i++)
+            {
+                LletraCodi[i] = dtsCli.Tables[0].Rows[i][0].ToString();
+
+            }
+              
+            return LletraCodi;
+        }
+
+        private void DesxifrarNumLletra(int z, string[] Codinumero)
+        {
+            string[] numeroCodi = new string[26];
+            numeroCodi = CodiLletra();
+            String[] lletres = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+            string rutaFitxerDesgen = "C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Arxiu" + z + ".txt";
+            string rutaFitxer = "C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM"+z+".txt";
+           
+            StreamWriter XifratNums = new StreamWriter(rutaFitxer);
+
+            int j = 0, t = 4;
+            string cadena, cad;
+            bool verifica = false;
+            cadena = File.ReadAllText(rutaFitxerDesgen);
+            for (int i = 0; i < Codinumero.Length; i++)
+            {
+                cad = cadena.Substring(j, t);
+
+                for (int x = 0; x < numeroCodi.Length; x++)
+                {
+                        if (numeroCodi[x].Equals(cad))
+                        {
+                            XifratNums.Write(lletres[x]);
+                        x = 26;
+                        }
+                }
+                j += 4;
+            }
+            
+            XifratNums.Close();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            crearFitxers();
+            MessageBox.Show("Has generat una moguda que flipas");
+        }
+
+        public void concatArxius(string ruta)
+        {
+            string A1 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM1.txt");
+            string A2 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM2.txt");
+            string A3 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM3.txt");
+            string A4 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Dessifra\\HUILALIEM4.txt");
+
+            File.WriteAllText(ruta, A1 + A2 + A3 + A4);
+        }
+        
+    }
+}
---
+++ GenerarFitxers/GenerarFitxers/FrmXifrasio.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GenerarFitxers
+{
+    public partial class FrmXifrasio : Form
+    {
+        public FrmXifrasio()
+        {
+            InitializeComponent();
+        }
+        public String[] lletres = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        public String[] crearLletres(int numeroFitxer)
+        {
+            String rutaFi = "C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS" + numeroFitxer+".txt";
+            Random obj = new Random();
+           String[] lletresRandom = new String[1000000];
+            
+            String codi = "";
+            StreamWriter file = new StreamWriter(rutaFi, true);
+                for (int x = 0; x < lletresRandom.Length; x++)
+                {
+                    int randomNumero = obj.Next(0,25);
+                    codi = lletres[randomNumero];
+                    file.Write(codi);
+                    lletresRandom[x] = codi;
+                }
+                file.Close();
+            return lletresRandom;
+        }
+        private void crearFitxers() {
+            String[] lletresRandom = new String[16000000];
+            for (int i = 1; i< 5; i++) {
+               lletresRandom = crearLletres(i);
+                XifrarLLetraNum(lletresRandom, i);
+            }
+            concatArxius("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS.txt");
+        }
+        public string[] CodiLletraCreacio()
+        {
+            SqlConnection connexxion = new SqlConnection();
+            connexxion.ConnectionString = ConfigurationManager.ConnectionStrings["RepublicSystemConnectionString"].ConnectionString;
+            connexxion.Open();
+
+            DataSet dtsCli = new DataSet();
+
+            string query = "SELECT Numbers from InnerEncryptionData where IdInnerEncryption = 24"; // and b.Day = '" + Convert.ToDateTime(DateTime.Today).ToString("yyyy-MM-dd") + "'";
+           
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connexxion);
+            adapter.Fill(dtsCli);
+
+            string[] LletraCodi = new string[26];
+
+            for (int i = 0; i < LletraCodi.Length; i++)
+            {
+                
+                LletraCodi[i] = dtsCli.Tables[0].Rows[i][0].ToString();
+                
+            }
+
+            return LletraCodi;
+        }
+
+        private void XifrarLLetraNum(string [] crearLletres, int z)
+        {
+            string[] codiLletra = new string[26];
+
+            bool verifica;
+            string rutaFitxer = "C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\Arxiu" + z+".txt";
+            codiLletra = CodiLletraCreacio();
+            StreamWriter XifratNums = new StreamWriter(rutaFitxer);
+            for (int i = 0; i < crearLletres.Length; i++)
+            {
+                verifica = false;
+                for (int x = 0; x < lletres.Length && verifica == false; x++)
+                {
+                    if (crearLletres[i].Equals(lletres[x]))
+                    {
+                        XifratNums.Write(codiLletra[x]);
+                        verifica = true;
+                    }
+                }
+            }
+
+            XifratNums.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            crearFitxers();
+            MessageBox.Show("Has generat una moguda que flipas");
+        }
+
+        public void concatArxius(string ruta)
+        {
+            string A1 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS1.txt");
+            string A2 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS2.txt");
+            string A3 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS3.txt");
+            string A4 = File.ReadAllText("C:\\Users\\admin\\source\\repos\\GenerarFitxers\\Fitxers\\PACS4.txt");
+
+            File.WriteAllText(ruta, A1 + A2 + A3 + A4);
+        }
+
+    }
+}
